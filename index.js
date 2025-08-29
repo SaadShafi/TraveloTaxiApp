347154e89091bbeb5ef65a9640c495b60ce2028c
Saad Shafi <saadshafi12@gmail.com>
2025-08-30 04:58:52 +0500

    .

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
 import { name as appName } from './app.json';
-import RideDetails from './src/screens/Driver/RideDetails';
-import BankDetails from './src/screens/Driver/BankDetails';
-import NotificationScreen from './src/screens/Global/Notification'
-AppRegistry.registerComponent(appName, () => NotificationScreen);
+import DriverHome from './src/screens/Driver/Home';
+AppRegistry.registerComponent(appName, () => DriverHome);
